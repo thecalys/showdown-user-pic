379b5b9ed836f5f8d860a48221506b009107605e
Calys M. Andrade <122690915+thecalys@users.noreply.github.com>
2025-07-08 20:21:56 -0300

    fix bug
    also fix the order of the code, thus fixing another bug

--- showdown-sprite.user.js
+++ showdown-sprite.user.js
@@ -6,55 +6,53 @@
 // @author       thecalys
 // @license      MIT
 // @match        https://*.pokemonshowdown.com/*
-// @run-at       document-start
+// @run-at       document-end
 // @icon         https://icons.duckduckgo.com/ip2/joanwestenberg.com.ico
 // @grant        none
 // ==/UserScript==
 
-(async function () {
-  "use strict";
-  console.log(`${window.customSprite.LoggerPrefix} user script has been initiated.`);
-  
-  // TODO: Allow users to upload their own sprite(s)
-  const userSpriteURL =
-    "https://play.pokemonshowdown.com/sprites/trainers/theroyal.png";
-
-  const userSpriteSelector = ".userdetails > img";
-  let userSpriteElem = document.querySelector(userSpriteSelector);
-  const observer = new MutationObserver((mutations) => {
-    if (document.querySelector(userSpriteSelector)) {
-      userSpriteElem = document.querySelector(userSpriteSelector);
-      if (window.customSprite.DEBUG === true)
-        Logger(
-          "success",
-          `[ShowdownCustomSprite] User Sprite "img" element found. Custom sprite URL will be applied.`
-        );
-      userSpriteElem.src = userSpriteURL;
-    } else if (window.customSprite.DEBUG === true) {
-      Logger(
-        "error",
-        `%cUser Sprite "img" element could not be found%c. Observer will wait for the DOM to change.`
-      );
-    }
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-  });
-})();
-
 window.customSprite = { DEBUG: false, Logger: undefined };
 window.customSprite.LoggerPrefix = "[ShowdownCustomSprite]"
 window.customSprite.Logger = (style = "neutral", content) => {
-  let styles = {
-    error: "color: crimson; font-weight: bold",
-    success: "color: limegreen; font-weight: bold",
-    neutral: "color: gray; font-weight: normal",
-  };
+    let styles = {
+        error: "color: crimson; font-weight: bold",
+        success: "color: limegreen; font-weight: bold",
+        neutral: "color: gray; font-weight: normal",
+    };
 
-  console[style === "error" ? style : "log"](
-    content,
-    styles[style] || undefined
-  );
+    console[style === "error" ? style : "log"](
+        content,
+        styles[style] || undefined,
+        style.neutral
+    );
 };
+
+
+(async function () {
+    "use strict";
+    window.customSprite.Logger("user script has been initiated.");
+
+    // TODO: Allow users to upload their own sprite(s)
+    const userSpriteURL =
+          "https://play.pokemonshowdown.com/sprites/trainers/theroyal.png";
+
+    const userSpriteSelector = ".trainersprite.yours";
+    let userSpriteElem = document.querySelector(userSpriteSelector);
+    const observer = new MutationObserver((mutations) => {
+        if (document.querySelector(userSpriteSelector)) {
+            userSpriteElem = document.querySelector(userSpriteSelector);
+            if (window.customSprite.DEBUG === true) window.customSprite.Logger("success", `%cUser Sprite "img" element found%c. Custom sprite URL will be applied.`);
+            userSpriteElem.src = userSpriteURL;
+        } else if (window.customSprite.DEBUG === true) {
+            window.customSprite.Logger(
+                "error",
+                `%cUser Sprite "img" element could not be found%c. Observer will wait for the DOM to change.`
+      );
+    }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+})();
